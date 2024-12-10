d0f29246d2fd4175da92e6bb800d940c766e8064
RobCoCoder <narek.ver0511@gmail.com>
2024-12-10 15:43:26 -0600

    Main frame setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 package src;
 
-public class App {
+import javax.swing.*;
+import javax.tools.Tool;
+import java.awt.*;
 
+public class App {
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            var frame = new AppFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
+
+class AppFrame extends JFrame {
+    static int frameWidth;
+    static int frameHeight;
+    Toolkit kit = Toolkit.getDefaultToolkit();
+
+    public AppFrame(){
+        Dimension screenSize = kit.getScreenSize();
+        frameWidth = (int) (screenSize.width / 2.2);
+        frameHeight = (int) (screenSize.height / 1.1);
+
+        setSize(frameWidth, frameHeight);
+        setLocationRelativeTo(null);
+    }
+}
+
